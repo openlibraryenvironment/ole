47a741d02bbd5cac32fd941de3e877324a248bf7
jcaddel <jcaddel@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-05-08 09:42:55 +0000

    Automated Impex SQL Update

---
+++ ole-app/ole-db/ole-sql/ole-ls-sql/src/main/resources/sql/mysql/OLE_CAT_GEN_RTN_POL_T.sql
@@ -0,0 +1,29 @@
+TRUNCATE TABLE OLE_CAT_GEN_RTN_POL_T
+/
+INSERT INTO OLE_CAT_GEN_RTN_POL_T (GEN_RTN_POL_CD,GEN_RTN_POL_ID,GEN_RTN_POL_NM,OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,VER_NBR)
+  VALUES ('0',1,'Unknown','1000','Y','MFHD 008-12 http://www.loc.gov/marc/holdings/hd008.html',STR_TO_DATE( '20120322000000', '%Y%m%d%H%i%s' ),1)
+/
+INSERT INTO OLE_CAT_GEN_RTN_POL_T (GEN_RTN_POL_CD,GEN_RTN_POL_ID,GEN_RTN_POL_NM,OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,VER_NBR)
+  VALUES ('1',2,'Other general retention policy','1001','Y','MFHD 008-12 http://www.loc.gov/marc/holdings/hd008.html',STR_TO_DATE( '20120322000000', '%Y%m%d%H%i%s' ),1)
+/
+INSERT INTO OLE_CAT_GEN_RTN_POL_T (GEN_RTN_POL_CD,GEN_RTN_POL_ID,GEN_RTN_POL_NM,OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,VER_NBR)
+  VALUES ('2',3,'Retained except as replaced by updates','1002','Y','MFHD 008-12 http://www.loc.gov/marc/holdings/hd008.html',STR_TO_DATE( '20120322000000', '%Y%m%d%H%i%s' ),1)
+/
+INSERT INTO OLE_CAT_GEN_RTN_POL_T (GEN_RTN_POL_CD,GEN_RTN_POL_ID,GEN_RTN_POL_NM,OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,VER_NBR)
+  VALUES ('3',4,'Sample issue retained','1003','Y','MFHD 008-12 http://www.loc.gov/marc/holdings/hd008.html',STR_TO_DATE( '20120322000000', '%Y%m%d%H%i%s' ),1)
+/
+INSERT INTO OLE_CAT_GEN_RTN_POL_T (GEN_RTN_POL_CD,GEN_RTN_POL_ID,GEN_RTN_POL_NM,OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,VER_NBR)
+  VALUES ('4',5,'Retained until replaced by microform','1004','Y','MFHD Leader 17 Encoding Level: http://www.loc.gov/marc/holdings/hdleader.html',STR_TO_DATE( '20120322000000', '%Y%m%d%H%i%s' ),1)
+/
+INSERT INTO OLE_CAT_GEN_RTN_POL_T (GEN_RTN_POL_CD,GEN_RTN_POL_ID,GEN_RTN_POL_NM,OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,VER_NBR)
+  VALUES ('5',6,'Retained until replaced by cumulation, replacement volume, or revision','1006','Y','MFHD 008-12 http://www.loc.gov/marc/holdings/hd008.html',STR_TO_DATE( '20120322000000', '%Y%m%d%H%i%s' ),1)
+/
+INSERT INTO OLE_CAT_GEN_RTN_POL_T (GEN_RTN_POL_CD,GEN_RTN_POL_ID,GEN_RTN_POL_NM,OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,VER_NBR)
+  VALUES ('6',7,'Retained for a limited period','1007','Y','MFHD 008-12 http://www.loc.gov/marc/holdings/hd008.html',STR_TO_DATE( '20120322000000', '%Y%m%d%H%i%s' ),1)
+/
+INSERT INTO OLE_CAT_GEN_RTN_POL_T (GEN_RTN_POL_CD,GEN_RTN_POL_ID,GEN_RTN_POL_NM,OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,VER_NBR)
+  VALUES ('7',8,'Not retained','1008','Y','MFHD 008-12 http://www.loc.gov/marc/holdings/hd008.html',STR_TO_DATE( '20120322000000', '%Y%m%d%H%i%s' ),1)
+/
+INSERT INTO OLE_CAT_GEN_RTN_POL_T (GEN_RTN_POL_CD,GEN_RTN_POL_ID,GEN_RTN_POL_NM,OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,VER_NBR)
+  VALUES ('8',9,'Permanently retained','1009','Y','MFHD 008-12 http://www.loc.gov/marc/holdings/hd008.html',STR_TO_DATE( '20120322000000', '%Y%m%d%H%i%s' ),1)
+/
