b93e8198dba8666d948a70ba4973761fc1a9e863
jcaddel <jcaddel@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-05-08 09:44:27 +0000

    Automated Impex SQL Update

---
+++ ole-app/ole-db/ole-sql/ole-master-sql/src/main/resources/sql/oracle/FP_DV_PMT_REAS_T.sql
@@ -0,0 +1,23 @@
+TRUNCATE TABLE FP_DV_PMT_REAS_T DROP STORAGE
+/
+INSERT INTO FP_DV_PMT_REAS_T (DV_PMT_REAS_CD,OBJ_ID,VER_NBR,DV_PMT_REAS_NM,DV_PMT_REAS_DESC,DV_PMT_REAS_ACTV_IND)
+  VALUES ('B','190BB72869E30028E043814FD8810028',1.0,'Reimbursement for Out of Pocket Expense','Reimbursement for Out of Pocket Expenses may be made to University employees.  These reimbursements are paid to an individual who incurs out-of-pocket expenses on behalf of their departmental business operations due to an emergency.  This payment reason may not be used in lieu of the procurement process.  A reimbursement should not exceed a total of $500.00.  No travel, meals, or personal service payments may be made using this payment reason.','Y')
+/
+INSERT INTO FP_DV_PMT_REAS_T (DV_PMT_REAS_CD,OBJ_ID,VER_NBR,DV_PMT_REAS_NM,DV_PMT_REAS_DESC,DV_PMT_REAS_ACTV_IND)
+  VALUES ('E','190BB72869E50028E043814FD8810028',1.0,'Compensation for Services','Compensations for Services can be used for Non-employees only.  These services include artist fees and honoraria.  Some common object codes would include 4500, 4503, 4535, 4561, 4024.','Y')
+/
+INSERT INTO FP_DV_PMT_REAS_T (DV_PMT_REAS_CD,OBJ_ID,VER_NBR,DV_PMT_REAS_NM,DV_PMT_REAS_DESC,DV_PMT_REAS_ACTV_IND)
+  VALUES ('F','190BB72869E60028E043814FD8810028',1.0,'Refund/Repayment to Individual or Agency','Refund/Repayment to Individual or Agency are payments made to individuals for returned goods sold, or services rendered, by the University.  This reason can also include repayment to agencies for contract and/or grant funding.  This payment reason may not be used for refunds of fees, tuition or residence hall payments.  Object codes would include income object codes and some of the 59XX refund object codes.','Y')
+/
+INSERT INTO FP_DV_PMT_REAS_T (DV_PMT_REAS_CD,OBJ_ID,VER_NBR,DV_PMT_REAS_NM,DV_PMT_REAS_DESC,DV_PMT_REAS_ACTV_IND)
+  VALUES ('G','190BB72869E70028E043814FD8810028',1.0,'Utilities, Freight or Postage','Payments for Utilities, Freight, and Postage can be made to non-employees only.  These payments can be made for telephone service, utilities, and postage, etc.  Common object codes would include 4085, 4086, 4093, 4097, 4098, 4300, 5027, 5028.','Y')
+/
+INSERT INTO FP_DV_PMT_REAS_T (DV_PMT_REAS_CD,OBJ_ID,VER_NBR,DV_PMT_REAS_NM,DV_PMT_REAS_DESC,DV_PMT_REAS_ACTV_IND)
+  VALUES ('K','190BB72869E90028E043814FD8810028',1.0,'Revolving Fund Reimbursement','Revolving funds are a kind of cash account from which funds can be borrowed when cash on hand is needed for disbursements. Revolving funds are established as special types of payees that can be repaid using a DV document.','Y')
+/
+INSERT INTO FP_DV_PMT_REAS_T (DV_PMT_REAS_CD,OBJ_ID,VER_NBR,DV_PMT_REAS_NM,DV_PMT_REAS_DESC,DV_PMT_REAS_ACTV_IND)
+  VALUES ('L','190BB72869EA0028E043814FD8810028',1.0,'Payments for Contractual Agreements','Used to make payments required under a contractual agreement.','Y')
+/
+INSERT INTO FP_DV_PMT_REAS_T (DV_PMT_REAS_CD,OBJ_ID,VER_NBR,DV_PMT_REAS_NM,DV_PMT_REAS_DESC,DV_PMT_REAS_ACTV_IND)
+  VALUES ('W','190BB72869F00028E043814FD8810028',1.0,'Subscriptions,Books,FEES,Resale','Subscriptions, Books and/or Membership Fees may be used for any payment covering a renewal subscription to a newspaper, magazine or cover membership fee to a professional association. When using this reason for subscriptions, please include a ship to address in the check stub text area for the vendor''s convenience.  Common object codes would include 4055, 4166, 5050, 5365.','Y')
+/
