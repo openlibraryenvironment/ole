edab13c362e5b2fc9acb2a8e7dafd4563398cd8a
pbalu <pbalu@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-05-12 08:00:55 +0000

    Removed the sql file since ole-sql-generator-staging failed due to OLE_DS_DOC_FIELD_T.sql is out of date

--- ole-app/ole-db/ole-sql/ole-ls-sql/src/main/resources/sql/mysql/OLE_CAT_SRC_TRM_T.sql
+++
@@ -1,8 +0,0 @@
-TRUNCATE TABLE OLE_CAT_SRC_TRM_T
-/
-INSERT INTO OLE_CAT_SRC_TRM_T (OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,SRC_TRM_CD,SRC_TRM_ID,SRC_TRM_NM,VER_NBR)
-  VALUES ('a4b4faa4-1b1c-4a0b-80bc-e2137f593e88','Y','http://www.loc.gov/standards/sourcelist/resource-action.html',STR_TO_DATE( '20120322120000', '%Y%m%d%H%i%s' ),'pda','3','Preservation & digitization actions: terminology',1)
-/
-INSERT INTO OLE_CAT_SRC_TRM_T (OBJ_ID,ROW_ACT_IND,SRC,SRC_DT,SRC_TRM_CD,SRC_TRM_ID,SRC_TRM_NM,VER_NBR)
-  VALUES ('aafce01c-c970-4b59-90a0-8c1990f3a622','Y','http://www.loc.gov/standards/sourcelist/resource-action.html',STR_TO_DATE( '20120322120000', '%Y%m%d%H%i%s' ),'stmanf','4','Standard terminology for the MARC 21 Actions',1)
-/
