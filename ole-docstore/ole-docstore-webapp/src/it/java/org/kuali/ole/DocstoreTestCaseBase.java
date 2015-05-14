eb936bd09a26506a237a4a4db8fbf601a9b59f41
sheiksalahudeen.m <sheiksalahudeen.m@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-05-14 13:47:45 +0000

    Modified for Jira OLE-4300. Merged the Docstore IT test case configuration to 2.0 (Trunk) code.

---
+++ ole-docstore/ole-docstore-webapp/src/it/java/org/kuali/ole/DocstoreTestCaseBase.java
@@ -0,0 +1,54 @@
+package org.kuali.ole;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kuali.rice.core.api.config.property.ConfigurationService;
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.krad.dao.DocumentDao;
+import org.kuali.rice.krad.service.impl.DocumentServiceImpl;
+import org.quartz.Scheduler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by sheiksalahudeenm on 7/4/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:org/kuali/ole/TestBootStrapSpringBeans.xml" })
+@TransactionConfiguration(transactionManager="transactionManager",defaultRollback=true)
+@Transactional
+public class DocstoreTestCaseBase {
+    protected final Logger LOG = LoggerFactory.getLogger(this.getClass());
+    protected String classesDir;
+
+    @Autowired
+    private ConfigurableApplicationContext context;
+
+    protected ConfigurableApplicationContext getContext() {
+        return context;
+    }
+
+    public DocstoreTestCaseBase() {
+        classesDir = getClass().getResource("/").getPath();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void applicationContextNotNull() throws Exception {
+        assertNotNull(context);
+    }
+}
