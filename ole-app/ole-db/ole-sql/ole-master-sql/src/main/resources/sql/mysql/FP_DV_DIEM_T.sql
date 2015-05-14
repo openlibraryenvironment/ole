7644e69ec42af8c7eee832af1c7797a949f80a94
jcaddel <jcaddel@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-05-14 06:50:59 +0000

    Automated Impex SQL Update

---
+++ ole-app/ole-db/ole-sql/ole-master-sql/src/main/resources/sql/mysql/FP_DV_DIEM_T.sql
@@ -0,0 +1,20 @@
+TRUNCATE TABLE FP_DV_DIEM_T
+/
+INSERT INTO FP_DV_DIEM_T (UNIV_FISCAL_YR,DV_DIEM_CNTRY_NM,OBJ_ID,VER_NBR,DV_DIEM_CNTRY_TXT)
+  VALUES (2014.0,'Foreign','fb875fb3-a03e-4e49-b8c1-c9295f458eba',1.0,'http://www.gsa.gov/Portal/gsa/ep/contentView.do?contentType=GSA_BASIC&contentId=17943&noc=T')
+/
+INSERT INTO FP_DV_DIEM_T (UNIV_FISCAL_YR,DV_DIEM_CNTRY_NM,OBJ_ID,VER_NBR,DV_DIEM_CNTRY_TXT)
+  VALUES (2014.0,'U.S. 48','f6abb8ae-4d71-4592-b357-bd8e1419914d',1.0,'http://www.gsa.gov/Portal/gsa/ep/contentView.do?contentType=GSA_BASIC&contentId=17943&noc=T')
+/
+INSERT INTO FP_DV_DIEM_T (UNIV_FISCAL_YR,DV_DIEM_CNTRY_NM,OBJ_ID,VER_NBR,DV_DIEM_CNTRY_TXT)
+  VALUES (2014.0,'US-AK & HI','b100344c-cb20-47ed-b573-0489e90dbb37',1.0,'http://www.gsa.gov/Portal/gsa/ep/contentView.do?contentType=GSA_BASIC&contentId=17943&noc=T')
+/
+INSERT INTO FP_DV_DIEM_T (UNIV_FISCAL_YR,DV_DIEM_CNTRY_NM,OBJ_ID,VER_NBR,DV_DIEM_CNTRY_TXT)
+  VALUES (2015.0,'Foreign','226a8654-6b06-4533-8042-2e0774d2d3ce',1.0,'http://www.gsa.gov/Portal/gsa/ep/contentView.do?contentType=GSA_BASIC&contentId=17943&noc=T')
+/
+INSERT INTO FP_DV_DIEM_T (UNIV_FISCAL_YR,DV_DIEM_CNTRY_NM,OBJ_ID,VER_NBR,DV_DIEM_CNTRY_TXT)
+  VALUES (2015.0,'U.S. 48','76e5c698-a403-4914-b8ad-8d82ca263c36',1.0,'http://www.gsa.gov/Portal/gsa/ep/contentView.do?contentType=GSA_BASIC&contentId=17943&noc=T')
+/
+INSERT INTO FP_DV_DIEM_T (UNIV_FISCAL_YR,DV_DIEM_CNTRY_NM,OBJ_ID,VER_NBR,DV_DIEM_CNTRY_TXT)
+  VALUES (2015.0,'US-AK & HI','e6eb2913-ee7c-4ccf-8d28-2a3b25cbedec',1.0,'http://www.gsa.gov/Portal/gsa/ep/contentView.do?contentType=GSA_BASIC&contentId=17943&noc=T')
+/
