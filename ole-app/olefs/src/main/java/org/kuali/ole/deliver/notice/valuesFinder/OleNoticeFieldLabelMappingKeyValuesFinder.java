14e7b84ac9bf0dc68ea63ec8341f489f964280e1
maheswaran.g <maheswaran.g@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-07-09 10:10:04 +0000

    Modified for the jira -OLE-7740 .Have provided the additional values in the drop-down.

--- ole-app/olefs/src/main/java/org/kuali/ole/deliver/notice/valuesFinder/OleNoticeFieldLabelMappingKeyValuesFinder.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/deliver/notice/valuesFinder/OleNoticeFieldLabelMappingKeyValuesFinder.java
@@ -21,13 +21,17 @@ public class OleNoticeFieldLabelMappingKeyValuesFinder extends KeyValuesBase {
         keyValues.add(new ConcreteKeyValue("Email","Email"));
         keyValues.add(new ConcreteKeyValue("Phone #","Phone #"));
         keyValues.add(new ConcreteKeyValue("Title/Item Information","Title/Item Information"));
-
         keyValues.add(new ConcreteKeyValue("Title","Title"));
         keyValues.add(new ConcreteKeyValue("Author","Author"));
         keyValues.add(new ConcreteKeyValue("Volume/Issue/Copy #","Volume/Issue/Copy #"));
         keyValues.add(new ConcreteKeyValue("Call #","Call #"));
         keyValues.add(new ConcreteKeyValue("Item Barcode","Item Barcode"));
-
+        keyValues.add(new ConcreteKeyValue("Original Due Date","Original Due Date"));
+        keyValues.add(new ConcreteKeyValue("New Due Date","New Due Date"));
+        keyValues.add(new ConcreteKeyValue("Item was due","Item was due"));
+        keyValues.add(new ConcreteKeyValue("Circulation Location/Library Name","Circulation Location/Library Name"));
+        keyValues.add(new ConcreteKeyValue("Circulation Reply-To Email","Circulation Reply-To Email"));
+        keyValues.add(new ConcreteKeyValue("Hold Expiration Date","Hold Expiration Date"));
         return keyValues;
     }
 }
