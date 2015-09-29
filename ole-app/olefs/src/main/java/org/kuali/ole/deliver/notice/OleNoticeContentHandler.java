8a56999e930367a92f6aac3c4c1641d4ae9c5483
peris <peris@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-09-29 16:34:51 +0000

    updated the notice template and fixed test case.

--- ole-app/olefs/src/main/java/org/kuali/ole/deliver/notice/OleNoticeContentHandler.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/deliver/notice/OleNoticeContentHandler.java
@@ -1,11 +1,23 @@
 package org.kuali.ole.deliver.notice;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.apache.commons.io.FilenameUtils;
+import org.codehaus.plexus.util.FileUtils;
+import org.kuali.ole.deliver.batch.OleNoticeBo;
 import org.kuali.ole.deliver.bo.OLEDeliverNoticeHistory;
 import org.kuali.rice.krad.service.BusinessObjectService;
 import org.kuali.rice.krad.service.KRADServiceLocator;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by pvsubrah on 9/28/15.
@@ -13,6 +25,48 @@ import java.util.List;
 public class OleNoticeContentHandler {
 
     private BusinessObjectService businessObjectService;
+
+    public String generateHTML(String noticeType, OleNoticeBo oleNoticeBo) {
+        StringWriter htmlContent = new StringWriter();
+        Configuration cfg = new Configuration();
+        try {
+
+            String noticeTemplateDirPath = System.getProperty("notice.template.dir");
+
+            File noticeTemplateDir = null;
+            if(null == noticeTemplateDirPath){
+                URI uri = getClass().getResource("notice.ftl").toURI();
+                File templateFile = new File(uri);
+                String tempDir = System.getProperty("java.io.tmpdir");
+                File destinationDirectory = new File(tempDir);
+                FileUtils.copyFileToDirectory(templateFile, destinationDirectory);
+                noticeTemplateDir = destinationDirectory;
+            } else {
+                noticeTemplateDir = new File(noticeTemplateDirPath);
+            }
+            cfg.setDirectoryForTemplateLoading(noticeTemplateDir);
+
+            Template template = cfg.getTemplate("notice.ftl");
+
+            Map<String, Object> input = new HashMap<>();
+
+            input.put("title", noticeType);
+
+            input.put("dueDate", oleNoticeBo.getDueDate().toString());
+            input.put("oleNoticeBo", oleNoticeBo);
+
+            template.process(input, htmlContent);
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+
+        return htmlContent.toString();
+    }
 
     public String getNoticeContent(String patronId) {
 
@@ -29,7 +83,7 @@ public class OleNoticeContentHandler {
 
     public BusinessObjectService getBusinessObjectService() {
         if (null == businessObjectService) {
-            businessObjectService =  KRADServiceLocator.getBusinessObjectService();
+            businessObjectService = KRADServiceLocator.getBusinessObjectService();
         }
         return businessObjectService;
     }
---
+++ ole-app/olefs/src/main/resources/org/kuali/ole/deliver/notice/notice.ftl
@@ -0,0 +1,78 @@
+<HTML>
+<TITLE>${title}</TITLE>
+<HEAD></HEAD>
+<BODY>
+
+<table>
+    </BR></BR>
+    <TR>
+        <TD>Patron Name :</TD>
+        <TD>${oleNoticeBo.patronName}</TD>
+    </TR>
+    <TR>
+        <TD>Address :</TD>
+        <TD>${oleNoticeBo.patronAddress}</TD>
+    </TR>
+    <TR>
+        <TD>EMAIL :</TD>
+        <TD>${oleNoticeBo.patronEmailAddress}</TD>
+    </TR>
+    <TR>
+        <TD>Phone Number :</TD>
+        <TD>${oleNoticeBo.patronPhoneNumber}</TD>
+    </TR>
+</table>
+
+<br/>
+<br/>
+
+<table width=\"100%\">
+    <TR>
+        <TD>
+            <CENTER>${title}</CENTER>
+        </TD>
+    </TR>
+    <TR>
+        <TD>
+            <p>
+            ${oleNoticeBo.noticeSpecificContent}
+            </p>
+        </TD>
+    </TR>
+</table>
+
+<br/>
+<br/>
+
+<table>
+    <TR>
+        <TD>Title :</TD>
+        <TD>${oleNoticeBo.title}</TD>
+    </TR>
+    <TR>
+        <TD>Author :</TD>
+        <TD>${oleNoticeBo.author}</TD>
+    </TR>
+    <TR>
+        <TD>Volume/Issue/Copy Number :</TD>
+        <TD>${oleNoticeBo.volumeNumber}</TD>
+    </TR>
+    <TR>
+        <TD>Item was due :</TD>
+        <TD>${dueDate}</TD>
+    </TR>
+    <TR>
+        <TD>Library shelving location :</TD>
+        <TD>${oleNoticeBo.itemShelvingLocation}</TD>
+    </TR>
+    <TR>
+        <TD>Call Number :</TD>
+        <TD>${oleNoticeBo.itemCallNumber}</TD>
+    </TR>
+    <TR>
+        <TD>Item Barcode :</TD>
+        <TD>${oleNoticeBo.itemId}</TD>
+    </TR>
+</table>
+</BODY>
+</HTML>
