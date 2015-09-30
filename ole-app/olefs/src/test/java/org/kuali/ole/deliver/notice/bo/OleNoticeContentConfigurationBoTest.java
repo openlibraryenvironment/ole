509916bcf8a939fa6ec13c2ea8f57d57f31a25de
peris <peris@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-09-30 13:28:14 +0000

    New test class for OleNoticeContentConfiguration

---
+++ ole-app/olefs/src/test/java/org/kuali/ole/deliver/notice/bo/OleNoticeContentConfigurationBoTest.java
@@ -0,0 +1,49 @@
+package org.kuali.ole.deliver.notice.bo;
+
+import org.junit.Test;
+import org.kuali.ole.OLEConstants;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pvsubrah on 9/30/15.
+ */
+public class OleNoticeContentConfigurationBoTest {
+
+    @Test
+    public void getLabel() throws Exception {
+        OleNoticeContentConfigurationBo oleNoticeContentConfigurationBo = new OleNoticeContentConfigurationBo();
+        oleNoticeContentConfigurationBo.setActive(true);
+        oleNoticeContentConfigurationBo.setNoticeName("OverdueNotice");
+        oleNoticeContentConfigurationBo.setNoticeTitle("OverdueNotice");
+        oleNoticeContentConfigurationBo.setNoticeType("OverdueNotice");
+
+        ArrayList<OleNoticeFieldLabelMapping> oleNoticeFieldLabelMappings = new ArrayList<>();
+        OleNoticeFieldLabelMapping patronName = new OleNoticeFieldLabelMapping();
+        patronName.setFieldLabel("Patron Full Name:");
+        patronName.setFieldName(OLEConstants.PATRON_NAME);
+
+
+        OleNoticeFieldLabelMapping patronEmail = new OleNoticeFieldLabelMapping();
+        patronEmail.setFieldLabel("Valid Email Id:");
+        patronEmail.setFieldName(OLEConstants.NOTICE_EMAIL);
+
+
+        OleNoticeFieldLabelMapping itemCallNum = new OleNoticeFieldLabelMapping();
+        itemCallNum.setFieldLabel("Item Call Number:");
+        itemCallNum.setFieldName(OLEConstants.NOTICE_CALL_NUMBER);
+
+        oleNoticeFieldLabelMappings.add(patronName);
+
+        oleNoticeContentConfigurationBo.setOleNoticeFieldLabelMappings(oleNoticeFieldLabelMappings);
+
+        String fieldLabel = oleNoticeContentConfigurationBo.getFieldLabel(OLEConstants.PATRON_NAME);
+        assertEquals("Patron Full Name:", fieldLabel);
+
+        String volumeLabel = oleNoticeContentConfigurationBo.getFieldLabel(OLEConstants.VOLUME_ISSUE_COPY);
+        assertEquals(OLEConstants.VOLUME_ISSUE_COPY, volumeLabel);
+    }
+
+}
