41ace1b960b7cc5d1844d9913ad96196ab921337
sheiksalahudeen.m <sheiksalahudeen.m@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-10-20 18:09:26 +0000

    Testcase for spliting the list based on chunk size

--- ole-app/olefs/src/it/java/org/kuali/ole/deliver/notice/solr/OleNoticeContentReIndexer_IT.java
+++ ole-app/olefs/src/it/java/org/kuali/ole/deliver/notice/solr/OleNoticeContentReIndexer_IT.java
@@ -1,8 +1,12 @@
 package org.kuali.ole.deliver.notice.solr;
 
 import org.apache.solr.client.solrj.response.UpdateResponse;
+import org.apache.solr.common.SolrInputDocument;
 import org.junit.Test;
 import org.kuali.ole.OLETestCaseBase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -16,5 +20,28 @@ public class OleNoticeContentReIndexer_IT extends OLETestCaseBase {
         UpdateResponse updateResponse = new OleNoticeContentReIndexer().reindexNoticeContent();
         assertNotNull(updateResponse);
         System.out.println(updateResponse.getElapsedTime());
+    }
+
+    @Test
+    public void chunkSizeTest() throws Exception {
+        List<String> stringList = new ArrayList<>();
+        for(int index = 0 ; index <= 1000 ; index++){
+            stringList.add("String : " + index);
+        }
+
+        int startingIndex = 0;
+        int chunkSize = 100;
+        int endIndex = chunkSize;
+        while(startingIndex <= stringList.size()){
+            if(endIndex > stringList.size()){
+                endIndex = stringList.size();
+            }
+            System.out.print("Staring Index : " + startingIndex + "    EndIndex : " + endIndex);
+            List<String> subList = stringList.subList(startingIndex, endIndex);
+            System.out.print(" SubList Size : " + subList.size());
+            System.out.println("\n");
+            startingIndex = startingIndex + chunkSize;
+            endIndex = endIndex + chunkSize;
+        }
     }
 }
