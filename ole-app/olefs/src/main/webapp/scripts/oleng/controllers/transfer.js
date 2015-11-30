c4504851c82c987e63b2ff603d234d9a27e12ef1
sriramsubramanianv <mail2sriram.sv@gmail.com>
2015-11-30 20:20:01 +0530

    Angularjs-TransferTree-Pagination

---
+++ ole-app/olefs/src/main/webapp/scripts/oleng/controllers/transfer.js
@@ -0,0 +1,128 @@
+var app = angular.module("transferApp", ['ui.tree']);
+
+
+app.controller('searchController', ['$scope', '$http', '$rootScope', searchConditions]);
+function searchConditions($scope, $http, $rootScope) {
+    $rootScope.baseUri = 'http://localhost:8080/oledocstore/bib/select?q='
+    $rootScope.searched = true;
+    $scope.rows = 10;
+    $scope.start = 0;
+    $scope.pageSize = 10;
+    $scope.showNext = false;
+    $scope.showPrevious = false;
+    $scope.totalRecords = 0;
+    $scope.documentTypes = [
+        {id: 'bibliographic', name: 'Bibliographic'},
+        {id: 'eHoldings', name: 'EHoldings'},
+        {id: 'holdings', name: 'Holdings'},
+        {id: 'item', name: 'Item'},
+        {id: 'license', name: 'License'}
+    ];
+
+    $scope.searchTypes = [
+        {id: 'search', name: 'Search'},
+        {id: 'browse', name: 'Browse'}
+    ];
+
+    $scope.searchScopes = [
+        {id: 'AND', name: 'All of these'},
+        {id: 'OR', name: 'Any of these'},
+        {id: 'phrase', name: 'Any of these'}
+    ];
+
+    $scope.inDoctypes = [
+        {id: 'bibliographic', name: 'Bibliographic'},
+        {id: 'eHoldings', name: 'EHoldings'},
+        {id: 'holdings', name: 'Holdings'},
+        {id: 'item', name: 'Item'}
+    ];
+
+    $scope.inFields = [
+        {id: 'any', name: 'ANY'},
+        {id: 'LocalId_search', name: 'Local Identifier'},
+        {id: 'Title_search', name: 'Title'}
+    ];
+
+    $scope.conditions = [
+        {value: '', searchScope: '', inDocumentType: 'bibliographic', inField: '', documentType: 'bibliographic'}
+    ];
+
+    $scope.removeCondition = function (ConditionToRemove) {
+        var index = $scope.conditions.indexOf(ConditionToRemove);
+        $scope.conditions.splice(index, 1);
+    };
+
+    $scope.addCondition = function () {
+        $scope.conditions.push({type: '', value: ''});
+    };
+
+    $scope.search = function () {
+        angular.forEach($scope.conditions, function (condition) {
+            var url = " DocType:" + condition.inDocumentType;
+            $http.get($rootScope.baseUri + url + '&wt=json&fl=Title_display,Author_display,Publisher_display,id,holdingsIdentifier,DocType&rows=10').
+                success(function (data) {
+                    console.log(data.response.docs);
+                    $rootScope.searchResults = data.response.docs;
+                    $rootScope.searched = false;
+                    $scope.totalRecords = data.response.numFound;
+                    if( $scope.start > 0){
+                        $scope.showPrevious = true;
+                    }else{
+                        $scope.showPrevious = false;
+                    }
+                    if($scope.start + $scope.pageSize <= $scope.totalRecords){
+                        $scope.showNext = true;
+                    }else{
+                        $scope.showNext = false;
+                    }
+                });
+        }, "");
+    };
+
+    $scope.previous = function () {
+        angular.forEach($scope.conditions, function (condition) {
+            $scope.start = $scope.start - $scope.pageSize;
+            var url = " DocType:" + condition.inDocumentType;
+            $http.get($rootScope.baseUri + url + '&wt=json&fl=Title_display,Author_display,Publisher_display,id,holdingsIdentifier,DocType&start=' + $scope.start).
+                success(function (data) {
+                    console.log(data.response.docs);
+                    $rootScope.searchResults = data.response.docs;
+                    $rootScope.searched = false;
+                    if( $scope.start > 0){
+                        $scope.showPrevious = true;
+                    }else{
+                        $scope.showPrevious = false;
+                    }
+                    if($scope.start + $scope.pageSize <= $scope.totalRecords){
+                        $scope.showNext = true;
+                    }else{
+                        $scope.showNext = false;
+                    }
+                });
+        }, "");
+    };
+
+    $scope.next = function () {
+        $scope.start = $scope.start + $scope.pageSize;
+        angular.forEach($scope.conditions, function (condition) {
+            var url = " DocType:" + condition.inDocumentType;
+            $http.get($rootScope.baseUri + url + '&wt=json&fl=Title_display,Author_display,Publisher_display,id,holdingsIdentifier,DocType&start=' + $scope.start).
+                success(function (data) {
+                    console.log(data.response.docs);
+                    $rootScope.searchResults = data.response.docs;
+                    $rootScope.searched = false;
+                    if( $scope.start > 0){
+                        $scope.showPrevious = true;
+                    }else{
+                        $scope.showPrevious = false;
+                    }
+                    if($scope.start + $scope.pageSize <= $scope.totalRecords){
+                        $scope.showNext = true;
+                    }else{
+                        $scope.showNext = false;
+                    }
+                });
+        }, "");
+    };
+
+};
