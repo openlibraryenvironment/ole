8565d92a96cb48d41f635413641a0b1132e2fc5c
Peri Health <info@perihealth.com>
2015-12-07 14:27:53 -0500

    Ranamed BibDAO

--- ole-docstore/ole-docstore-webapp/src/main/java/org/kuali/ole/dsng/dao/BibDAO.java
+++ ole-docstore/ole-docstore-webapp/src/main/java/org/kuali/ole/dsng/dao/BibDAO.java
@@ -3,9 +3,8 @@ package org.kuali.ole.dsng.dao;
 import org.kuali.ole.docstore.engine.service.storage.rdbms.pojo.BibRecord;
 
 /**
- * Created by SheikS on 12/3/2015.
+ * Created by pvsubrah on 12/7/15.
  */
 public interface BibDAO {
-
     public BibRecord retrieveBibById(String id);
 }
