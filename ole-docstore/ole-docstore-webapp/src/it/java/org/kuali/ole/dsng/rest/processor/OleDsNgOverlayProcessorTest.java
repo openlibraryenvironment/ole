145bae66039790fc18b319d6672016938fbe9b61
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2015-12-08 14:42:26 -0500

    Added test case of serialize and deserialize the SolrInputDocument

--- ole-docstore/ole-docstore-webapp/src/it/java/org/kuali/ole/dsng/rest/processor/OleDsNgOverlayProcessorTest.java
+++ ole-docstore/ole-docstore-webapp/src/it/java/org/kuali/ole/dsng/rest/processor/OleDsNgOverlayProcessorTest.java
@@ -1,6 +1,8 @@
 package org.kuali.ole.dsng.rest.processor;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.solr.common.SolrInputDocument;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 import org.junit.Test;
@@ -13,7 +15,7 @@ import static org.junit.Assert.*;
 /**
  * Created by SheikS on 12/8/2015.
  */
-public class OleDsNgOverlayProcessorTest extends DocstoreTestCaseBase implements DocstoreConstants {
+public class OleDsNgOverlayProcessorTest implements DocstoreConstants {
 
     @Autowired
     OleDsNgRestAPIProcessor oleDsNgRestAPIProcessor;
@@ -29,5 +31,34 @@ public class OleDsNgOverlayProcessorTest extends DocstoreTestCaseBase implements
 
         String savedJSON = oleDsNgRestAPIProcessor.processOverlayForBib(jsonArray.toString());
         assertTrue(StringUtils.isNotBlank(savedJSON));
+    }
+
+    @Test
+    public void testSerializeAndDeserializeSolrInputDocumentAsJSON() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        SolrInputDocument solrInputDocument = new SolrInputDocument();
+        solrInputDocument.addField("id",10101);
+        solrInputDocument.addField("Title","Bib Title");
+        solrInputDocument.addField("Author","Bib Author");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", 10101);
+        jsonObject.put("solrInputDocuemnt", objectMapper.writeValueAsString(solrInputDocument));
+
+        String serializedContent = jsonObject.toString();
+        assertTrue(StringUtils.isNotBlank(serializedContent));
+        System.out.println(serializedContent);
+
+
+        JSONObject jsonObject1 = new JSONObject(serializedContent);
+        assertNotNull(jsonObject1);
+
+        String solrInputDocuemntContent = (String) jsonObject1.getString("solrInputDocuemnt");
+        SolrInputDocument deserializedDocument = objectMapper.readValue(solrInputDocuemntContent, SolrInputDocument.class);
+        assertNotNull(deserializedDocument);
+        System.out.println(deserializedDocument);
+
+
     }
 }
