955c8315259d5f931565912fa17c44e077dddb4f
premkumar <pbalu@kuali.org>
2015-12-10 20:58:52 +0530

    OLE-3904 - Made Bibimport report file downloadable

--- ole-app/olefs/src/main/java/org/kuali/ole/sys/OLEConstants.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/sys/OLEConstants.java
@@ -2007,6 +2007,7 @@ public class OLEConstants {
     public static final String OLE_COPY_SCHEMA = "OleCopyFiscalYearData.groovy";
     public static final String INVALID_FILE = "invalid.file";
     public static final String REENCUM_RECURR = "ReEncumberRecuring";
+    public static final String BIBIMPORT_FILE_PATTERN = "Job";
     public static final String ROLLOVER_DIRECTORY = "/rollover";
     public static final String PO_BULK_AMEND_OUT_FILE_NM = "POBA-Report.txt";
     public static final String POBA_DIRECTORY = "/poba";
