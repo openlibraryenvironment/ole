cb4832a3d24a053b70bec736befdd63fa000892d
Peri Health <info@perihealth.com>
2015-12-10 11:56:37 -0500

    Renamed method.

--- ole-app/olefs/src/main/java/org/kuali/ole/spring/batch/processor/BatchBibFileProcessor.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/spring/batch/processor/BatchBibFileProcessor.java
@@ -1,6 +1,7 @@
 package org.kuali.ole.spring.batch.processor;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.solr.common.SolrDocument;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
@@ -13,9 +14,7 @@ import org.marc4j.marc.Record;
 import org.marc4j.marc.Subfield;
 import org.marc4j.marc.VariableField;
 
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by SheikS on 12/9/2015.
@@ -37,28 +36,37 @@ public class BatchBibFileProcessor extends BatchFileProcessor {
                     SolrRequestReponseHandler solrRequestReponseHandler = new SolrRequestReponseHandler();
                     List results = solrRequestReponseHandler.getSolrDocumentList("mdf_980a:" + "\"" + matchPoint1 + "\"");
                     if (null != results && results.size() == 1) {
-                        JSONObject jsonObject = new JSONObject();
+                        JSONObject bib = new JSONObject();
                         SolrDocument solrDocument = (SolrDocument) results.get(0);
                         String updatedUserName = getUpdatedUserName();
                         String updatedDate = DocstoreConstants.DOCSTORE_DATE_FORMAT.format(new Date());
-                        jsonObject.put("id", solrDocument.getFieldValue("LocalId_display"));
-                        jsonObject.put("content", generateMARCXMLContent(marcRecord));
-                        jsonObject.put("updatedBy", updatedUserName);
-                        jsonObject.put("updatedDate", DocstoreConstants.DOCSTORE_DATE_FORMAT.format(new Date()));
-                        List<String> holdingIds = (List<String>) solrDocument.get("holdingsIdentifier");
-                        if(CollectionUtils.isNotEmpty(holdingIds)) {
-                            JSONArray holdingsJsonArray = new JSONArray();
-                            for (Iterator<String> holdingsIdIterator = holdingIds.iterator(); holdingsIdIterator.hasNext(); ) {
-                                String holdingId = holdingsIdIterator.next();
-                                JSONObject holdingJsonObject = new JSONObject();
-                                holdingJsonObject.put("id", holdingId);
-                                holdingJsonObject.put("updatedBy", updatedUserName);
-                                holdingJsonObject.put("updatedDate", updatedDate);
-                                holdingsJsonArray.put(holdingJsonObject);
-                            }
-                            jsonObject.put("holdingIds",holdingsJsonArray);
-                        }
-                        jsonArray.put(jsonObject);
+
+                        //Bib data
+
+                        Map bibData = new HashMap<>();
+                        bibData.put("id", solrDocument.getFieldValue("LocalId_display"));
+                        bibData.put("content", generateMARCXMLContent(marcRecord));
+                        bibData.put("updatedBy", updatedUserName);
+                        bibData.put("updatedDate", updatedDate);
+
+
+                        bib.put("bib", bibData);
+
+
+                        JSONObject holdings = new JSONObject();
+                        Map holdingsData = new HashedMap();
+                        holdingsData.put("locationLevel1", "UC");
+                        holdingsData.put("locationLevel3", "UCX");
+                        holdingsData.put("locationLevel5", "InProc");
+                        holdingsData.put("callNumberType", "LCC - Library Of Congress classification");
+
+
+                        holdings.put("holdings", holdingsData);
+
+                        //Holdings data
+
+                        jsonArray.put(bibData);
+                        jsonArray.put(holdingsData);
                     }
                 }
             }
