c721e7360e971b0fff59810777521c23d6d20254
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2015-12-28 10:24:40 -0500

    Fixed merge conflict

--- ole-docstore/ole-docstore-webapp/src/main/java/org/kuali/ole/dsng/service/SpringContext.java
+++ ole-docstore/ole-docstore-webapp/src/main/java/org/kuali/ole/dsng/service/SpringContext.java
@@ -6,7 +6,6 @@ import org.springframework.context.ApplicationContext;
  * Created by SheikS on 12/23/2015.
  */
 public class SpringContext {
-
     public static Object getBean(String beanId) {
         ApplicationContext applicationContext = ApplicationContextProvider.getInstance().getApplicationContext();
         return applicationContext.getBean(beanId);
