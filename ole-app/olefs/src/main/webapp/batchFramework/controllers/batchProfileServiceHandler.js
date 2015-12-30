d28c3cedbbc0df58de7c9929c499bb0f77831947
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2015-12-30 01:25:10 -0500

    Added the row level edit for the collections.

--- ole-app/olefs/src/main/webapp/batchFramework/controllers/batchProfileServiceHandler.js
+++ ole-app/olefs/src/main/webapp/batchFramework/controllers/batchProfileServiceHandler.js
@@ -122,3 +122,48 @@ getMaintenanceData = function (dataObject, $scope, $http) {
         }
     }
 };
+
+
+getMaintenanceDataForBibImport = function (fieldType, $scope, $http) {
+    if ((fieldType == 'Call Number Type' || fieldType == 'Holdings Call Number Type') && $scope.callNumberTypeValues == undefined) {
+        $http.get(OLENG_CONSTANTS.PROFILE_GET_CALLNUMBER_TYPES).success(function (data) {
+            $scope.callNumberTypeValues = data;
+        });
+    } else if (fieldType == 'Item Type' && $scope.itemTypeValues == undefined) {
+        $http.get(OLENG_CONSTANTS.PROFILE_GET_ITEM_TYPES).success(function (data) {
+            $scope.itemTypeValues = data;
+        });
+    } else if (fieldType == 'Item Status' && $scope.itemStatusValues == undefined) {
+        $http.get(OLENG_CONSTANTS.PROFILE_GET_ITEM_STATUS).success(function (data) {
+            $scope.itemStatusValues = data;
+        });
+    } else if (fieldType == 'Donor Code' && $scope.donorCodeValues == undefined) {
+        $http.get(OLENG_CONSTANTS.PROFILE_GET_DONOR_CODES).success(function (data) {
+            $scope.donorCodes = data;
+        });
+    } else if ((fieldType == 'Location Level1' || fieldType == 'Holdings Location Level1') && $scope.locationLevel1Values == undefined) {
+        $http.get(OLENG_CONSTANTS.PROFILE_GET_LOCATIONS, {params: {"levelId": 1}}).success(function (data) {
+            $scope.locationLevel1Values = data;
+        });
+    } else if ((fieldType == 'Location Level2' || fieldType == 'Holdings Location Level2') && $scope.locationLevel2Values == undefined) {
+        $http.get(OLENG_CONSTANTS.PROFILE_GET_LOCATIONS, {params: {"levelId": 2}}).success(function (data) {
+            $scope.locationLevel2Values = data;
+        });
+    } else if ((fieldType == 'Location Level3' || fieldType == 'Holdings Location Level3') && $scope.locationLevel3Values == undefined) {
+        $http.get(OLENG_CONSTANTS.PROFILE_GET_LOCATIONS, {params: {"levelId": 3}}).success(function (data) {
+            $scope.locationLevel3Values = data;
+        });
+    } else if ((fieldType == 'Location Level4' || fieldType == 'Holdings Location Level4') && $scope.locationLevel4Values == undefined) {
+        $http.get(OLENG_CONSTANTS.PROFILE_GET_LOCATIONS, {params: {"levelId": 4}}).success(function (data) {
+            $scope.locationLevel4Values = data;
+        });
+    } else if ((fieldType == 'Location Level5' || fieldType == 'Holdings Location Level5') && $scope.locationLevel5Values == undefined) {
+        $http.get(OLENG_CONSTANTS.PROFILE_GET_LOCATIONS, {params: {"levelId": 5}}).success(function (data) {
+            $scope.locationLevel5Values = data;
+        });
+    } else if (fieldType == 'Bib Status' && $scope.bibStatuses == undefined) {
+        $http.get(OLENG_CONSTANTS.PROFILE_GET_BIB_STATUS).success(function (data) {
+            $scope.bibStatuses = data;
+        });
+    }
+};
