1a0e49d17961c889c3aaec898d0ea270dd3a91be
Peri Health <info@perihealth.com>
2016-01-05 09:53:15 -0500

    Refactored addstephandler to handle move; Will need to write a new handler for "new"

--- ole-app/olefs/src/main/java/org/kuali/ole/spring/batch/handlers/AddOperationStepHandler.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/spring/batch/handlers/AddOperationStepHandler.java
@@ -67,7 +67,7 @@ public class AddOperationStepHandler extends StepHandler {
 
     @Override
     public Boolean isInterested(String operation) {
-        return operation.equalsIgnoreCase("add");
+        return operation.equalsIgnoreCase("move");
 
     }
 }
