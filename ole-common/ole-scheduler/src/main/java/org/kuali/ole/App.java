4d8b57a8f855345a918477b8251f073fa66cba73
Simon Ritter <simon.b.ritter@gmail.com>
2016-01-12 15:15:58 +0100

    Create new project ole-scheduler and update .gitignore for Eclipse

---
+++ ole-common/ole-scheduler/src/main/java/org/kuali/ole/App.java
@@ -0,0 +1,13 @@
+package org.kuali.ole;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
