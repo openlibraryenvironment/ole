9e33e465943accbc7a301f5ef1d954db7fc18a54
maheshgopal <maheswaran.g@htcindia.com>
2016-02-02 18:39:31 +0530

    Modified for jira OLE-7616 . Fix for setting the default request type and default pick up location as like in create new screen opf request

--- ole-app/olefs/src/main/java/org/kuali/ole/deliver/keyvalue/OlePlaceRequestCirculationDeskKeyValue.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/deliver/keyvalue/OlePlaceRequestCirculationDeskKeyValue.java
@@ -1,9 +1,13 @@
 package org.kuali.ole.deliver.keyvalue;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.kuali.ole.OLEConstants;
 import org.kuali.ole.deliver.bo.OleCirculationDesk;
+import org.kuali.ole.deliver.bo.OleCirculationDeskDetail;
 import org.kuali.ole.deliver.bo.OleCirculationDeskLocation;
 import org.kuali.ole.deliver.form.OLEPlaceRequestForm;
+import org.kuali.ole.deliver.service.ParameterValueResolver;
 import org.kuali.ole.describe.form.BoundwithForm;
 import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
@@ -11,10 +15,12 @@ import org.kuali.rice.krad.service.BusinessObjectService;
 import org.kuali.rice.krad.service.KRADServiceLocator;
 import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
 import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.rice.krad.util.GlobalVariables;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by maheswarang on 2/6/15.
@@ -22,6 +28,11 @@ import java.util.List;
 public class OlePlaceRequestCirculationDeskKeyValue extends UifKeyValuesFinderBase {
 
     private BusinessObjectService businessObjectService;
+
+    @Override
+    public boolean isAddBlankOption() {
+        return false;
+    }
 
     public BusinessObjectService getBusinessObjectService() {
         if(businessObjectService == null){
@@ -40,14 +51,59 @@ public class OlePlaceRequestCirculationDeskKeyValue extends UifKeyValuesFinderBa
         HashMap<String,String> circulationDeskMap = new HashMap<String,String>();
         circulationDeskMap.put("circulationPickUpDeskLocation",olePlaceRequestForm.getItemLocation());
         List<OleCirculationDeskLocation> oleCirculationDeskLocations = (List<OleCirculationDeskLocation>)getBusinessObjectService().findMatching(OleCirculationDeskLocation.class,circulationDeskMap);
+        OleCirculationDesk defaultPickUpLocation = getDefaultPickUpLocation();
+         if(defaultPickUpLocation == null){
+             defaultPickUpLocation = getOperatorDefaultPickupLocation();
+        }
         List<KeyValue> options = new ArrayList<KeyValue>();
         //TODO: Get drop-down values dynamically by parsing DocumentConfig.xml file
+        if(defaultPickUpLocation!=null){
+            options.add(new ConcreteKeyValue(defaultPickUpLocation.getCirculationDeskId(),defaultPickUpLocation.getCirculationDeskCode()));
+        }else{
+            options.add(new ConcreteKeyValue("",""));
+        }
           if(oleCirculationDeskLocations!=null && oleCirculationDeskLocations.size()>0){
-
           for(OleCirculationDeskLocation oleCirculationDeskLocation : oleCirculationDeskLocations){
-            options.add(new ConcreteKeyValue(oleCirculationDeskLocation.getCirculationDeskId(),oleCirculationDeskLocation.getOleCirculationDesk().getCirculationDeskCode()));
-          }
+              if(defaultPickUpLocation!=null){
+              if(!oleCirculationDeskLocation.getCirculationDeskId().equals(defaultPickUpLocation.getCirculationDeskId())){
+               options.add(new ConcreteKeyValue(oleCirculationDeskLocation.getCirculationDeskId(),oleCirculationDeskLocation.getOleCirculationDesk().getCirculationDeskCode()));
+              }
+              }else{
+                options.add(new ConcreteKeyValue(oleCirculationDeskLocation.getCirculationDeskId(),oleCirculationDeskLocation.getOleCirculationDesk().getCirculationDeskCode()));
+              }
+              }
           }
         return options;
+    }
+
+    private OleCirculationDesk getDefaultPickUpLocation() {
+        String parameterValue = ParameterValueResolver.getInstance().getParameter(OLEConstants.APPL_ID_OLE, OLEConstants.DLVR_NMSPC, OLEConstants.DLVR_CMPNT, OLEConstants.DEFAULT_PICK_UP_LOCATION);
+        if (StringUtils.isNotBlank(parameterValue)) {
+            Map<String, String> pickUpLocMap = new HashMap<>();
+            pickUpLocMap.put(OLEConstants.PICKUP_LOCATION, "true");
+            pickUpLocMap.put(OLEConstants.OleCirculationDesk.OLE_CIRCULATION_DESK_CD, parameterValue);
+            List<OleCirculationDesk> oleCirculationDesks = (List<OleCirculationDesk>) KRADServiceLocator.getBusinessObjectService().findMatching(OleCirculationDesk.class, pickUpLocMap);
+            if (CollectionUtils.isNotEmpty(oleCirculationDesks)) {
+                return oleCirculationDesks.get(0);
+            }
+        }
+        return null;
+    }
+
+
+    private OleCirculationDesk getOperatorDefaultPickupLocation(){
+        OleCirculationDesk oleCirculationDesk = null;
+        Map<String,String> criteriaMap = new HashMap<String,String>();
+        criteriaMap.put("operatorId", GlobalVariables.getUserSession().getPrincipalId());
+        criteriaMap.put("defaultLocation","Y");
+        List<OleCirculationDeskDetail> oleCirculationDeskDetailList = (List<OleCirculationDeskDetail>)getBusinessObjectService().findMatching(OleCirculationDeskDetail.class,criteriaMap);
+        if(oleCirculationDeskDetailList.size()>0){
+          OleCirculationDeskDetail  oleCirculationDeskDetail = oleCirculationDeskDetailList.get(0);
+          if(oleCirculationDeskDetail.getOleCirculationDesk().getDefaultPickupLocation().isPickUpLocation()){
+            oleCirculationDesk = oleCirculationDeskDetail.getOleCirculationDesk().getDefaultPickupLocation();
+          }
+
+        }
+        return oleCirculationDesk;
     }
 }
