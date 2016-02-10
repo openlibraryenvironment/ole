7f7e9db71b7be0d31cd9dc1c2454c5b389f23d43
maheshgopal <maheswaran.g@htcindia.com>
2016-02-10 16:30:11 +0530

    Fix for the request bugs

--- ole-app/olefs/src/main/java/org/kuali/ole/deliver/keyvalue/PickupLocationKeyValueFinder.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/deliver/keyvalue/PickupLocationKeyValueFinder.java
@@ -9,15 +9,15 @@ import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
 import org.kuali.rice.krad.keyvalues.KeyValuesBase;
 import org.kuali.rice.krad.service.KRADServiceLocator;
+import org.kuali.rice.krad.uif.control.UifKeyValuesFinderBase;
 
 import java.util.*;
 
 /**
  * Created by angelind on 7/27/15.
  */
-public class PickupLocationKeyValueFinder extends KeyValuesBase {
+public class PickupLocationKeyValueFinder extends UifKeyValuesFinderBase {
 
-    private LoanProcessor loanProcessor = new LoanProcessor();
 
     @Override
     public List<KeyValue> getKeyValues() {
@@ -25,35 +25,11 @@ public class PickupLocationKeyValueFinder extends KeyValuesBase {
         Map<String, String> userMap = new HashMap<String, String>();
         userMap.put(OLEConstants.PICKUP_LOCATION, "true");
         Collection<OleCirculationDesk> oleCirculationDesks = KRADServiceLocator.getBusinessObjectService().findMatching(OleCirculationDesk.class, userMap);
-        OleCirculationDesk circulationDesk = validateDefaultPickUpLocation();
-        if (circulationDesk != null) {
-            options.add(new ConcreteKeyValue(circulationDesk.getCirculationDeskId(),circulationDesk.getCirculationDeskCode()));
-            for (OleCirculationDesk oleCirculationDesk : oleCirculationDesks) {
-                if (!circulationDesk.getCirculationDeskId().equals(oleCirculationDesk.getCirculationDeskId())) {
-                    options.add(new ConcreteKeyValue(oleCirculationDesk.getCirculationDeskId(), oleCirculationDesk.getCirculationDeskCode()));
-                }
-            }
-        } else {
-            options = new ArrayList<KeyValue>();
-            options.add(new ConcreteKeyValue("", ""));
-            for (OleCirculationDesk oleCirculationDesk : oleCirculationDesks) {
-                options.add(new ConcreteKeyValue(oleCirculationDesk.getCirculationDeskId(), oleCirculationDesk.getCirculationDeskCode()));
-            }
-        }
+     if(oleCirculationDesks.size()>0){
+         for(OleCirculationDesk oleCirculationDesk : oleCirculationDesks){
+             options.add(new ConcreteKeyValue(oleCirculationDesk.getCirculationDeskId(),oleCirculationDesk.getCirculationDeskCode()));
+         }
+     }
         return options;
-    }
-
-    private OleCirculationDesk validateDefaultPickUpLocation() {
-        String parameterValue = loanProcessor.getParameter(OLEConstants.DEFAULT_PICK_UP_LOCATION);
-        if (StringUtils.isNotBlank(parameterValue)) {
-            Map<String, String> pickUpLocMap = new HashMap<>();
-            pickUpLocMap.put(OLEConstants.PICKUP_LOCATION, "true");
-            pickUpLocMap.put(OLEConstants.OleCirculationDesk.OLE_CIRCULATION_DESK_CD, parameterValue);
-            List<OleCirculationDesk> oleCirculationDesks = (List<OleCirculationDesk>) KRADServiceLocator.getBusinessObjectService().findMatching(OleCirculationDesk.class, pickUpLocMap);
-            if (CollectionUtils.isNotEmpty(oleCirculationDesks)) {
-                return oleCirculationDesks.get(0);
-            }
-        }
-        return null;
     }
 }
