aa5729a0fdedec47a4159b65f7e350c3c66fd757
Simon Ritter <Simon Ritter>
2016-02-16 17:39:05 +0100

    Minor corrections

--- ole-common/ole-scheduler/src/main/java/org/kuali/ole/scheduler/BatchJobLauncher.java
+++ ole-common/ole-scheduler/src/main/java/org/kuali/ole/scheduler/BatchJobLauncher.java
@@ -32,7 +32,7 @@ public class BatchJobLauncher {
 
     public void start() {
         try {
-            System.out.println("--- start batchannotation job ---");
+            System.out.println("--- start batch job ---");
 
             FileBody fileBody = new FileBody(new File(getClass().getResource("sample-marc").toURI()),
                     ContentType.APPLICATION_OCTET_STREAM, "sample-marc");
--- ole-common/ole-scheduler/src/main/java/org/kuali/ole/scheduler/BatchJobScheduler.java
+++ ole-common/ole-scheduler/src/main/java/org/kuali/ole/scheduler/BatchJobScheduler.java
@@ -28,19 +28,19 @@ public class BatchJobScheduler {
     }
 
     /**
-     * Create a new batchannotation job and register it in the scheduler
+     * Create a new batch job and register it in the scheduler
      * 
      * @param jobName
      *            the name of the job
      * @param triggerName
      *            the name of its trigger
      * @param profileId
-     *            the ole batchannotation profile id
+     *            the ole batch profile id
      * @param batchType
-     *            the ole batchannotation job type
+     *            the ole batch job type
      * @param url
      *            the url of the rest controller to call, this is where the
-     *            actual batchannotation job starts
+     *            actual batch job starts
      * @param cronExpression
      *            the cron expression to specify when the job should run
      * @throws Exception
