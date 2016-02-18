592bd47f0d9339299bbedfc5dc30d44f3d55c3c3
rajeshbabuk <rajeshbabu.k@htcindia.com>
2016-02-18 21:24:16 +0530

    Batch Profile Inquiry

--- ole-app/olefs/src/main/webapp/framework/angular/js/customDirectives.js
+++ ole-app/olefs/src/main/webapp/framework/angular/js/customDirectives.js
@@ -7,7 +7,7 @@ customDirectivesApp.directive('modal', function () {
     return {
         template: '<div class="modal fade">' +
         '<div class="modal-dialog">' +
-        '<div id="modalContentId" class="modal-content" style="width: 750px">' +
+        '<div id="modalContentId" class="modal-content">' +
         '<div class="modal-header">' +
         '<button type="button" class="close" data-dismiss="modal" aria-hidden="true" ng-click="closeModal()">&times;</button>' +
         '<h4 class="modal-title">{{ title }}</h4>' +
