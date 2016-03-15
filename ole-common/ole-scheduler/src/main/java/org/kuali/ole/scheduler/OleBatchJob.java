bbe93c231647a9d123261e84f32a3844d5ee61ed
Simon Ritter <simon.b.ritter@gmail.com>
2016-03-15 19:03:45 +0100

    Work with real Spring Batch Jobs
    Still messy but working
    
    Need to clean this up

--- ole-common/ole-scheduler/src/main/java/org/kuali/ole/scheduler/OleBatchJob.java
+++ ole-common/ole-scheduler/src/main/java/org/kuali/ole/scheduler/OleBatchJob.java
@@ -1,20 +1,30 @@
 package org.kuali.ole.scheduler;
 
-import org.springframework.batch.core.Job;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Date;
 
 /**
  * Created by SBRitter on 07.03.2016.
  */
-public class OleBatchJob {
+public class OleBatchJob implements Runnable {
 
     private Job job;
     private String cronExpression;
     private String name;
+    private JobLauncher jobLauncher;
 
-    public OleBatchJob(Job job, String cronExpression, String name) {
+    public OleBatchJob(Job job, String cronExpression, String name, JobLauncher jobLauncher) {
         this.job = job;
         this.cronExpression = cronExpression;
         this.name = name;
+        this.jobLauncher = jobLauncher;
     }
 
     public Job getJob() {
@@ -39,5 +49,30 @@ public class OleBatchJob {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public JobLauncher getJobLauncher() {
+        return jobLauncher;
+    }
+
+    public void setJobLauncher(JobLauncher jobLauncher) {
+        this.jobLauncher = jobLauncher;
+    }
+
+    @Override
+    public void run() {
+        try {
+            JobParameters jobParameters = new JobParametersBuilder().addDate("run date", new Date()).toJobParameters();
+            JobExecution execution = jobLauncher.run(job, jobParameters);
+            System.out.println("Exit Status: " + execution.getStatus());
+        } catch (JobExecutionAlreadyRunningException e) {
+            e.printStackTrace();
+        } catch (JobRestartException e) {
+            e.printStackTrace();
+        } catch (JobInstanceAlreadyCompleteException e) {
+            e.printStackTrace();
+        } catch (JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
     }
 }
--- ole-common/ole-scheduler/src/main/java/org/kuali/ole/scheduler/SpringCronJob.java
+++ ole-common/ole-scheduler/src/main/java/org/kuali/ole/scheduler/SpringCronJob.java
@@ -1,41 +1,166 @@
 package org.kuali.ole.scheduler;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.kuali.ole.constants.OleNGConstants;
+import org.kuali.rice.core.api.config.property.ConfigContext;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class SpringCronJob extends HttpServlet implements Runnable {
 
     private ThreadPoolTaskScheduler scheduler;
+    String[] springConfig = {"spring/batch/jobs/*.xml"};
+    ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
+    JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+    private String urlBase;
 
     public void init() throws ServletException {
         // invoke in separate thead to complete the deployment in parallel
         new Thread(new Runnable() {
             @Override
             public void run() {
-                start();
+                try {
+                    start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }).start();
     }
 
-    public void start() {
+    public void start() throws IOException, JSONException, InterruptedException {
         System.err.println("SpringCronJob.start()");
         scheduler = new ThreadPoolTaskScheduler();
         scheduler.setThreadNamePrefix("SpringCronJob");
         scheduler.initialize();
 
-        scheduler.schedule(this, new CronTrigger("0/3 * * * * ?"));
+        System.err.println("-- initializing --");
+        List<OleBatchJob> allJobs = null;
+        for (int n = 1; n < 50; n++) {
+            allJobs = getAllScheduledJobs();
+            if (allJobs != null) {
+                break;
+            }
+            System.err.println("rest not yet available, not fully deployed?");
+            TimeUnit.SECONDS.sleep(n);
+        }
+
+        if (allJobs == null) {
+            throw new RuntimeException("REST api not available: " + OleNGConstants.BATCH_PROCESS_JOBS);
+        }
+
+        scheduleJobs(allJobs);
+    }
+
+    private void scheduleJobs(List<OleBatchJob> allJobs) {
+        for (OleBatchJob oleBatchJob : allJobs) {
+            scheduler.schedule(oleBatchJob, new CronTrigger(oleBatchJob.getCronExpression()));
+        }
     }
 
     public void run() {
         System.err.println("SpringCronJob.run()");
     }
 
-    public static void main(String[] args) {
-        SpringCronJob job = new SpringCronJob();
-        job.start();
+    List<OleBatchJob> getAllScheduledJobs() throws IOException, JSONException {
+        String schedulesString = rest(OleNGConstants.BATCH_PROCESS_JOBS);
+        if (schedulesString == null) {
+            return null;
+        }
+        JSONArray schedules = new JSONArray(schedulesString);
+        List<OleBatchJob> jobs = new ArrayList<>(schedules.length());
+        for (int i = 0; i < schedules.length(); i++) {
+            try {
+                JSONObject schedule = schedules.getJSONObject(i);
+                String name = schedule.optString(OleNGConstants.PROCESS_NAME);
+                String cron = schedule.optString(OleNGConstants.CRON_EXPRESSION);
+                Job springBatchJob = (Job) context.getBean(name);
+                OleBatchJob oleBatchJob = new OleBatchJob(springBatchJob, cron, name, jobLauncher);
+                jobs.add(oleBatchJob);
+                System.err.println("added oleBatchJob: " + name + " " + cron + " " + schedule.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return jobs;
     }
+
+    /**
+     * Set the urlBase, this can be used for regression testing when the ConfigContext
+     * hasn't been initialized.
+     *
+     * @param urlBase URL excluding the trailing slash
+     */
+    void setUrlBase(String urlBase) {
+        this.urlBase = urlBase;
+    }
+
+    private String getUrlBase() {
+        if (urlBase != null) {
+            return urlBase;
+        }
+        urlBase = ConfigContext.getCurrentContextConfig().getProperty("ole.fs.url.base");
+        return urlBase;
+    }
+
+    /**
+     * Do a http Get at some rest URL.
+     *
+     * @param restPath the path starting with "rest/" of the URL where to do the GET
+     * @return response to the Get request, null if restPath not found
+     */
+    private String rest(String restPath) throws IOException {
+        String url = getUrlBase() + "/" + restPath;
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("ole-quickstart", ""));
+        CloseableHttpClient httpClient = HttpClientBuilder
+                .create()
+                .setDefaultCredentialsProvider(credentialsProvider)
+                .build();
+        HttpResponse response = httpClient.execute(new HttpGet(url));
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_FOUND) {
+            return null;
+        }
+        InputStream body = response.getEntity().getContent();
+        Scanner scanner = new Scanner(body, "UTF-8").useDelimiter("\\A");
+        if (!scanner.hasNext()) {
+            return "";
+        }
+        return scanner.next();
+    }
+
+//    public static void main(String[] args) {
+//        SpringCronJob job = new SpringCronJob();
+//        job.start();
+//    }
+
 }
