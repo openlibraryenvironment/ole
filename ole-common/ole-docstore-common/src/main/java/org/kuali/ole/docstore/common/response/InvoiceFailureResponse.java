15e9fe438b3359704ca8c02437aea75042ec69d4
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2016-03-16 22:50:37 -0400

    Refactored the code.

--- ole-common/ole-docstore-common/src/main/java/org/kuali/ole/docstore/common/response/InvoiceFailureResponse.java
+++ ole-common/ole-docstore-common/src/main/java/org/kuali/ole/docstore/common/response/InvoiceFailureResponse.java
@@ -4,14 +4,4 @@ package org.kuali.ole.docstore.common.response;
  * Created by angelind on 3/15/16.
  */
 public class InvoiceFailureResponse extends FailureResponse {
-
-    private String invoiceNumber;
-
-    public String getInvoiceNumber() {
-        return invoiceNumber;
-    }
-
-    public void setInvoiceNumber(String invoiceNumber) {
-        this.invoiceNumber = invoiceNumber;
-    }
 }
