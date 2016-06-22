cab4ec318bcf3fb8121ff7153c1a435ba54b88db
jayared <jayapred@gmail.com>
2016-06-22 15:51:51 +0530

    OLE-8931 - New Batch import delete and replace eholdings is deleting eholdings with POs attached

--- ole-common/ole-utility/src/main/java/org/kuali/ole/constants/OleNGConstants.java
+++ ole-common/ole-utility/src/main/java/org/kuali/ole/constants/OleNGConstants.java
@@ -352,5 +352,6 @@ public class OleNGConstants {
     public static final String MONTHLY = "Monthly";
 
     public static final String _VERSION_ = "_version_";
+    public final static String ERR_HOLDINGS_HAS_REQ_OR_PO = "Holdings record is linked with Requisition/PO";
 
 }
