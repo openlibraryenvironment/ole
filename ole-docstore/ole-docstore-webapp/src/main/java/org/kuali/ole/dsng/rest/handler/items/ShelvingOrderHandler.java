575cb733a2bb70d646495a924f3812ee3618f9ea
girisankar <girisankar@htcindia.com>
2016-11-21 13:18:55 +0530

    OLE-9036 	 Bib import not finishing holdings and item records

--- ole-docstore/ole-docstore-webapp/src/main/java/org/kuali/ole/dsng/rest/handler/items/ShelvingOrderHandler.java
+++ ole-docstore/ole-docstore-webapp/src/main/java/org/kuali/ole/dsng/rest/handler/items/ShelvingOrderHandler.java
@@ -7,6 +7,7 @@ import org.codehaus.jettison.json.JSONObject;
 import org.kuali.ole.Exchange;
 import org.kuali.ole.constants.OleNGConstants;
 import org.kuali.ole.docstore.engine.service.storage.rdbms.pojo.HoldingsRecord;
+import org.kuali.ole.docstore.engine.service.storage.rdbms.pojo.ItemRecord;
 import org.kuali.ole.dsng.rest.handler.holdings.HoldingsHandler;
 
 import java.util.Iterator;
@@ -23,8 +24,8 @@ public class ShelvingOrderHandler extends ItemHandler  {
         List<String> listFromJSONArray = getListFromJSONArray(jsonArrayeFromJsonObject.toString());
         if(CollectionUtils.isNotEmpty(listFromJSONArray)) {
             String shelvingOrder = listFromJSONArray.get(0);
-            HoldingsRecord holdingRecord = (HoldingsRecord) exchange.get(OleNGConstants.ITEM_RECORD);
-            holdingRecord.setShelvingOrder(shelvingOrder);
+            ItemRecord itemRecord = (ItemRecord) exchange.get(OleNGConstants.ITEM_RECORD);
+            itemRecord.setShelvingOrder(shelvingOrder);
         }
     }
 
@@ -35,12 +36,12 @@ public class ShelvingOrderHandler extends ItemHandler  {
 
     @Override
     public void process(JSONObject requestJsonObject, Exchange exchange) {
-        HoldingsRecord holdingRecord = (HoldingsRecord) exchange.get(OleNGConstants.ITEM_RECORD);
+        ItemRecord itemRecord = (ItemRecord) exchange.get(OleNGConstants.ITEM_RECORD);
         String shelvingOrder = getStringValueFromJsonObject(requestJsonObject, OleNGConstants.BatchProcess.SHELVING_ORDER);
         List<String> parsedValues = parseCommaSeperatedValues(shelvingOrder);
         for (Iterator<String> iterator = parsedValues.iterator(); iterator.hasNext(); ) {
             String shelvingOrderValue = iterator.next();
-            if(StringUtils.equals(holdingRecord.getCallNumberPrefix(), shelvingOrderValue)) {
+            if(StringUtils.equals(itemRecord.getCallNumberPrefix(), shelvingOrderValue)) {
                 exchange.add(OleNGConstants.ITEM_RECORD, Boolean.TRUE);
                 exchange.add(OleNGConstants.MATCHED_VALUE, shelvingOrderValue);
             }
