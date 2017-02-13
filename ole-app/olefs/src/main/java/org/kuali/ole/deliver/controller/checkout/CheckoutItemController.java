e7c693e77a7401b14da41632d63b8f61359157c6
girisankar <girisankar@htcindia.com>
2017-02-13 17:11:04 +0530

    OLE-9118    CR process should prompt operator if item has Requests

--- ole-app/olefs/src/main/java/org/kuali/ole/deliver/controller/checkout/CheckoutItemController.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/deliver/controller/checkout/CheckoutItemController.java
@@ -410,7 +410,7 @@ public class CheckoutItemController extends CircFastAddItemController {
         return getUIFModelAndView(circForm);
     }
 
-    public void createClaimsReturnForItem(CircForm circForm, List<OleLoanDocument> loanDocumentList, OlePatronDocument olePatronDocument) throws Exception {
+    public void createClaimsReturnForItem(CircForm circForm, List<OleLoanDocument> loanDocumentList, OlePatronDocument olePatronDocument,String cancelRequest) throws Exception {
         if (CollectionUtils.isNotEmpty(loanDocumentList)) {
             for (Iterator<OleLoanDocument> iterator = loanDocumentList.iterator(); iterator.hasNext(); ) {
                 OleLoanDocument oleLoanDocument = iterator.next();
@@ -423,7 +423,9 @@ public class CheckoutItemController extends CircFastAddItemController {
                 parameterMap.put("patronId", olePatronDocument.getOlePatronId());
                 parameterMap.put("proxyPatronId", oleLoanDocument.getProxyPatronId());
                 getCheckoutUIController(circForm.getFormKey()).updateItemInfoInSolr(parameterMap, oleLoanDocument.getItemUuid(), false);
-                new OleDeliverRequestDocumentHelperServiceImpl().cancelPendingRequestForClaimsReturnedItem(oleLoanDocument.getItemUuid());
+                if(cancelRequest!=null && cancelRequest.equalsIgnoreCase("true")){
+                    new OleDeliverRequestDocumentHelperServiceImpl().cancelPendingRequestForClaimsReturnedItem(oleLoanDocument.getItemUuid());
+                }
             }
         }
     }
