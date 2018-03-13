91e349a31babc9748ad134b7f1f883058272743f
NSSuresh11 <suresh.subramanian@kuali.org>
2018-03-13 13:44:52 +0530

    OLE-7724 : Number of pieces field in item record should be defined as number not free text

--- ole-app/olefs/src/main/webapp/ole/describe/scripts/editor.js
+++ ole-app/olefs/src/main/webapp/ole/describe/scripts/editor.js
@@ -21,6 +21,19 @@ jq(document).ready(function () {
         }
     });
 
+    jq("input#oleItemNumberOfPieces_control").live('keydown',function(event) {
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 ||
+            (event.keyCode == 65 && event.ctrlKey === true) ||
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+            return;
+        }
+        else {
+            if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault();
+            }
+        }
+    });
+
     window.focus = function(a){
 
         try{
