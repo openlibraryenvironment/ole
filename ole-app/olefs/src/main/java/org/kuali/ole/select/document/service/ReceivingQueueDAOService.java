cf18d09f3d92643bdf162c22a007434f2a62a1a6
NSSuresh11 <suresh.s@htcindia.com>
2018-08-09 12:14:39 +0530

    Revert: OLE-7660 : Slow response on Receiving and Claiming Queue searches

--- ole-app/olefs/src/main/java/org/kuali/ole/select/document/service/ReceivingQueueDAOService.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/select/document/service/ReceivingQueueDAOService.java
@@ -12,10 +12,6 @@ public interface ReceivingQueueDAOService {
 
     public List<Map<String, Object>> getPODetails(Map<String, Object> criteria);
 
-    public List<Map<String, Object>> getVendorPODetails(Map<String, Object> criteria);
-
     public List<OlePurchaseOrderDocument> getPODocumentList(Map<String, Object> criteria);
-
-    public List<OlePurchaseOrderDocument> getPOVendorDocumentList(Map<String, Object> criteria);
 
 }
