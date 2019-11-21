56c616e6c1f40808def53f4510c0edd5421d7388
NSSuresh11 <suresh.s@htcindia.com>
2019-11-21 11:31:44 +0530

    OLE-9443 : Loan Override Does Not Work If Patron Has Request Onhold

--- ole-app/olefs/src/main/java/org/kuali/ole/deliver/controller/checkout/CheckoutValidationController.java
+++ ole-app/olefs/src/main/java/org/kuali/ole/deliver/controller/checkout/CheckoutValidationController.java
@@ -1,6 +1,7 @@
 package org.kuali.ole.deliver.controller.checkout;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.kuali.ole.deliver.controller.PermissionsValidatorUtil;
 import org.kuali.ole.deliver.form.CircForm;
 import org.kuali.rice.krad.web.form.UifFormBase;
@@ -22,7 +23,10 @@ public class CheckoutValidationController extends CheckoutPatronController {
                                                   HttpServletRequest request, HttpServletResponse response) throws Exception {
         CircForm circForm = (CircForm) form;
         boolean hasValidOverridePermissions = new PermissionsValidatorUtil().hasValidOverridePermissions(circForm);
-        if(CollectionUtils.isEmpty(circForm.getErrorMessage().getPermissions())){
+        if(circForm.getItemBarcode() != null && StringUtils.isNotBlank(circForm.getItemBarcode())) {
+            hasValidOverridePermissions = Boolean.TRUE;
+        }
+        if(CollectionUtils.isEmpty(circForm.getErrorMessage().getPermissions()) && circForm.getItemBarcode() == null){
             return postPatronValidation(circForm, result, request, response);
         } else {
             if ((hasValidOverridePermissions)) {
